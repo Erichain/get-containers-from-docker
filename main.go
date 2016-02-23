2fb80c0c2c7191f1abbbd6d99607d98c6f787d44
zhengyuchuan <jyamol520@gmail.com>
2016-02-23 18:00:31 +0800

    add main functions, ready to be test
    Signed-off-by: zhengyuchuan <jyamol520@gmail.com>

--- main.go
+++ main.go
@@ -1,17 +1,66 @@
 package main
 
-import(
-	"fmt"
+import (
+	"net/url"
 	"net/http"
-	"runtime"
+	"encoding/json"
 )
 
-func indexHandler( w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "hello world, I'm running on %s with an %s CPU ", runtime.GOOS,runtime.GOARCH)
-
+// docker's ports
+type APIPort struct {
+	PrivatePort		int64
+	PublicPort		int64
+	Type			string
+	IP				string
 }
 
-func main(){
-	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":8080",nil)
+// the options of listing containers
+type ListContainersOptions struct {
+	All     bool
+	Limit   int
+	Since   string
+	Before  string
+	Size    bool
+	Filters map[string][]string
+}
+
+// the options each container represents
+type APIContainers struct {
+	ID			string		//`json:"Id" yaml:"Id"`
+	Names		[]string
+	Image		string
+	Command		string
+	Created		int64
+	Status		string
+	Ports		[]APIPort
+	Labels		map[string]string
+	SizeRw		int64
+	SizeRootFs	int64
+}
+
+func main() {
+	var options ListContainersOptions
+
+	options.All = true
+	options.Limit = 5
+
+
+	ListContainers(options)
+}
+
+func ListContainers(options ListContainersOptions) ([]APIContainers, error) {
+	path := "/containers/json?" + url.QueryEscape(options)
+	res, err := http.Get(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	var containers []APIContainers
+	if err := json.NewDecoder(res.Body).Decode(&containers); err != nil {
+		return nil, err
+	}
+
+	return containers, nil
 }
